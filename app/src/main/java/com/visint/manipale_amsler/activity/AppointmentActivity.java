7e9f20d13d6937f3cbbfa070edaee20a8e19ebec
darshanjagthap <darshandjagthap@gmail.com>
2016-06-01 14:55:07 +0530

    appointment

---
+++ app/src/main/java/com/visint/manipale_amsler/activity/AppointmentActivity.java
@@ -0,0 +1,101 @@
+package com.visint.manipale_amsler.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.fourmob.datetimepicker.date.DatePickerDialog;
+import com.fourmob.datetimepicker.date.DatePickerDialog.OnDateSetListener;
+import com.sleepbot.datetimepicker.time.RadialPickerLayout;
+import com.sleepbot.datetimepicker.time.TimePickerDialog;
+import com.visint.manipale_amsler.R;
+
+import java.util.Calendar;
+
+public class AppointmentActivity extends AppCompatActivity implements OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+
+    public static final String DATEPICKER_TAG = "datepicker";
+    public static final String TIMEPICKER_TAG = "timepicker";
+
+    CoordinatorLayout coordinatorLayout;
+    TextView jmsg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appointment);
+
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorLayout);
+
+        final Calendar calendar = Calendar.getInstance();
+
+        jmsg = (TextView) findViewById(R.id.msg);
+
+        jmsg.setText("");
+
+        final DatePickerDialog datePickerDialog = DatePickerDialog.newInstance(this, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), isVibrate());
+        final TimePickerDialog timePickerDialog = TimePickerDialog.newInstance(this, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false, false);
+
+        findViewById(R.id.book).setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                datePickerDialog.setVibrate(isVibrate());
+                datePickerDialog.setYearRange(1950, 2028);
+                datePickerDialog.setCloseOnSingleTapDay(isCloseOnSingleTapDay());
+                datePickerDialog.show(getSupportFragmentManager(), DATEPICKER_TAG);
+
+                timePickerDialog.setVibrate(isVibrate());
+                timePickerDialog.setCloseOnSingleTapMinute(isCloseOnSingleTapMinute());
+                timePickerDialog.show(getSupportFragmentManager(), TIMEPICKER_TAG);
+            }
+        });
+
+        if (savedInstanceState != null) {
+            DatePickerDialog dpd = (DatePickerDialog) getSupportFragmentManager().findFragmentByTag(DATEPICKER_TAG);
+            if (dpd != null) {
+                dpd.setOnDateSetListener(this);
+            }
+
+            TimePickerDialog tpd = (TimePickerDialog) getSupportFragmentManager().findFragmentByTag(TIMEPICKER_TAG);
+            if (tpd != null) {
+                tpd.setOnTimeSetListener(this);
+            }
+        }
+
+    }
+
+    private boolean isVibrate() {
+        return true;
+    }
+
+    private boolean isCloseOnSingleTapDay() {
+        return false;
+    }
+
+    private boolean isCloseOnSingleTapMinute() {
+        return false;
+    }
+
+    String date, time;
+
+    @Override
+    public void onDateSet(DatePickerDialog datePickerDialog, int year, int month, int day) {
+        date = year + "-" + month + "-" + day;
+    }
+
+    @Override
+    public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute) {
+        time = hourOfDay + "-" + minute;
+
+        jmsg.setText("Appointment book on " + date + " at " + time);
+
+        Snackbar.make(coordinatorLayout, "You will be notified soon", Snackbar.LENGTH_SHORT).show();
+    }
+}
+
